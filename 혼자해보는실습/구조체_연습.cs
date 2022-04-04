763b44a87a84b60a91680d6864a42fb9c127b381
MEAJIN <kimo3o98@gmail.com>
2022-04-04 21:00:20 +0900

    0404

---
+++ 혼자해보는실습/구조체_연습.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 혼자해보는_실습
+{
+    public struct Mystruct
+    {
+        public int 국어, 영어, 수학, 합계;
+        public float 평균;
+        public void Compute()
+        {
+            합계 = 국어 + 영어 + 수학;
+            평균 = 합계 / 3.0f;
+        }
+
+
+    }
+    internal class 구조체_연습
+    {
+        static void Main(string[] args)
+        {
+            Mystruct Test1 = new Mystruct();
+            Test1.국어 = 80;
+            Test1.영어 = 90;
+            Test1.수학 = 88;
+            Test1.Compute();
+            Console.WriteLine("총점 : {0}, 평균 : {1}", Test1.합계, Test1.평균);
+        }
+    }
+
+
+}
---
+++ 혼자해보는실습/열거형_연습.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 요일에 대한 열거형 번수를 정수형 변수에 대입하여 출력하여 보자.
+// 열거형? 상수를 문자열로 대치하여 선언
+namespace 혼자해보는_실습
+{
+    enum Days { sun = 1, Mon, Tue, Wed, Thu, Fri, Sat };
+    internal class 열거형_연습
+    {
+        static void Main(string[] args)
+        {
+            int nValue = (int)Days.Mon;
+            Days day = Days.Tue;
+            Console.WriteLine("{0}, {1}", nValue, day);
+        }
+    }
+}
