4297fffe0361dc5881eb03d1c2e8dcf2391f2276
MEAJIN <kimo3o98@gmail.com>
2022-05-03 23:45:41 +0900

    0503

---
+++ 혼자해보는실습/as연산자_연습.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_study
+{
+    class A
+    {
+
+    }
+
+    class B
+    {
+
+    }
+
+    internal class as연산자_연습
+    {
+        // 형변환과 변환 조사
+        // 캐스트 연산자의 역할과 불변환은 null 리턴
+        // 
+        // 형식
+        // 결과형 = 참조형, 언바싱, 박싱 as 변환형
+        
+        static void Main()
+        {
+            string str1 = "123";
+            object obj = str1;
+            string str2 = obj as string;
+            Console.WriteLine(str2);
+
+            A test1 = new A();
+            object obj1 = test1;
+            B test2 = obj1 as B;
+            if (test2 == null)
+                Console.WriteLine("형변환 실패");
+            else
+                Console.WriteLine("형변환 성공");
+
+
+        }
+    }
+}
---
+++ 혼자해보는실습/is연산자_연습.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_study
+{
+    internal class is연산자_연습
+    {
+        static void Main(string[] args)
+        {
+            // is연산자
+            // 형식 호환을 조사하는 연산자
+            //
+            // 형식 :
+            // A(변수) is B(클래스형 도는 데이터형)
+            // 결과는 true, false
+
+            int nValue = 10;
+            if (nValue is float)
+                Console.WriteLine("호환됨");
+            else
+                Console.WriteLine("호환 안 됨");
+
+            if (nValue is object)
+                Console.WriteLine("호환됨");
+            else
+                Console.WriteLine("호환 안 됨");
+
+            object obj = nValue;
+            if(obj is int)
+            {
+                Console.WriteLine("호환됨");
+            }
+            else
+            {
+                Console.WriteLine("호환 안 됨");
+            }
+        }
+    }
+}
